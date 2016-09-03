798c9680ba816acf93df86f89a12617cc6cf448d
leeofri <leeofri@gmail.com>
2016-09-03 12:20:41 -0400

    anather one bite the dust

--- src/solution/Globals.java
+++ src/solution/Globals.java
@@ -16,7 +16,7 @@ public class Globals {
     //folder in the  where to put the data in hdfs when we're done
     public static final String WORD_FREQ_OUTPUT_FOLDER = "WORD_FREQ";
 	    
-	static String outputFolder = "./output";
+	static String outputFolder = "./FromTheTweet/output";
 	
 	public static void updateCandidate (String candidateCode,int numOfTweets, Context context)
 	{
